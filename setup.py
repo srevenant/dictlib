fb36b5e9bbaff77bb69d71a4ae6f01de62679558
s3tqr2w <brandon.gillespie@kuali.co>
2016-07-02 08:17:59 -0600

    commit2

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ setup(
   name = 'dicts',
   packages = ['dicts'],
   version = '1.0',
-  description = 'Dictionary Tools including good deep merge and dictionary as objects'
-  author = 'Brandon Gillespie'
-  author_email = 'bjg-pypi@solv.com'
+  description = 'Dictionary Tools including good deep merge and dictionary as objects',
+  author = 'Brandon Gillespie',
+  author_email = 'bjg-pypi@solv.com',
   url = 'https://github.com/srevenant/dicts', 
   download_url = 'https://github.com/srevenant/dicts/tarball/1.0',
   keywords = ['dict', 'union', 'object'],
