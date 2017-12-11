e59e582e580a71744171d72624ffd604f3cbdd4b
brandon <brandon@cold.org>
2017-12-11 13:08:38 -0700

    update

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 setup(
   name = 'dictlib',
   packages = ['dictlib'],
-  version = '1.0',
+  version = '1.0.2',
   description = 'Yet another Dictionary Library including good deep merge and dictionary as objects',
   author = 'Brandon Gillespie',
-  author_email = 'bjg-pypi@solv.com',
+  author_email = 'bjg-pypi@cold.org',
   url = 'https://github.com/srevenant/dictlib', 
   download_url = 'https://github.com/srevenant/dictlib/tarball/1.0',
   keywords = ['dict', 'union', 'object'],
